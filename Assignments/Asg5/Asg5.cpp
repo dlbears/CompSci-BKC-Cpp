f5af8783238c580cee3f79daf14a81d744747135
dlbears <dennisorsini97@gmail.com>
2017-04-25 21:39:22 -0400

    Added utility function bodies, need to order in main and test

--- Assignments/Asg5/Asg5.cpp
+++ Assignments/Asg5/Asg5.cpp
@@ -3,38 +3,37 @@
 using namespace std;
 
 ifstream in("in.txt");
+ofstream out("out.txt");
 
-int readdata(int, double[]);
+void readdata(int, double[]);
 void printarray(int, double[]);
-int findaverage(int, double[]);
-int howfaraway(int, int, double[], double[]);
+double findaverage(int, double[]);
+void howfaraway(int, int, double[], double[]);
 
 int main() {
   int size;
-  in >> size;
-  double first[size];
+  double first[size], second[size], avg;
   
+  in >> size;
   readdata(size, first);
-  cout >> "~Heading~\n";
+  out << "~Heading~\n";
   printarray(size, first);
 }
 
-int readdata(int n, double[] numbers) {
-
-
+int readdata(int n, double numbers[]) {
+  for(int i = 0; i < n; i++) in >> numbers[i];
 }
 
-void printarray(int q, double[] numb) {
-
-
+void printarray(int q, double numb[]) {
+  for(int i = 0; i < q; i++) out << numbs[i] << (((i+1) % 5 == 0) ? "\n" : " ");
 }
 
-int findaverage(int k, double[] p) {
-
-
+double findaverage(int k, double p[]) {
+  double sum;
+  for(int i = 0; i < k; i++) sum += p[i];
+  return sum / k;
 }
 
-int howfaraway(int m, int avg, double[] r, double[] s) {
-
-
+void howfaraway(int m, int avg, double r[], double s[]) {
+  for(int i = 0; i < m; i++) s[i] = r[i] - avg;
 }

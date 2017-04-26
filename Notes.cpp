498834a35a342852ed8ac0e5b0adbb845b771207
dlbears <dennisorsini97@gmail.com>
2017-04-25 20:25:24 -0400

    String size type decleration, length function , and using string like arrays in a for loop

--- Notes.cpp
+++ Notes.cpp
@@ -1,4 +1,4 @@
-#include <string> // String type library
+#include <string> // standard String library
 #include <iostream> // Input-Output stream library
 #include <fstream> // File stream (IO stream but for files)
 using namespace std; // instead of constantly std::cout or std::cin
@@ -6,16 +6,27 @@ using namespace std; // instead of constantly std::cout or std::cin
 ofstream out("output.txt"); // output stream to a file "output", like cout but to a file
 ifstream in("input.txt"); // input stream like cin, but from a file
 int main() {
-    string name; //This is a string variable
-    
+    string name; // This is a string variable
     in >> name; // from file (each call goes down a line)
     out << name << endl; // to a file (on a new line)
+    // cin >> name; prompts for input from console 
+    // cout << name;  to console 
 
-    //cin >> name; prompts for input from console 
-    //cout << name;  to console 
 
-     //String concatination
+     // String concatination
+         name += " that is now concatenated";
+         out << name << endl; // Word that is now concatenated;
 
-     name += " that is now concatenated";
-     out << name << endl;
+
+     // String access/editing
+         name[0] = 'w'; // word that is now concatenated
+     
+     /* 
+     String length function returns the length of the string - \0 (at the end) 
+     string::size_type, subset of int that only allows positive values
+     */
+        string::size_type strLen = name.length(); 
+     
+     //Print out each char on its own line
+        for(int i = 0; i < strLen; i++) out << name[i] << endl;    
 }
